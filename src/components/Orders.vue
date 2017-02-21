c6418a2f1d54bacf288d9b82b96e2b75056c9e07
Hoang Nam <namhoang.td09@gmail.com>
2017-02-21 21:46:47 +0000

    update order component

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -1,16 +1,26 @@
 <template>
   <data-tables :data='tableData'
     :has-action-col='false'
+    :pagination-def='getPaginationDef()'
     @row-click='rowClick'>
-    <el-table-column
-      prop="orderID"
-      label="Order ID"
-      sortable="custom">
+
+    <el-table-column prop="orderID" label="Order ID" sortable="custom"></el-table-column>
+    <el-table-column prop="date" label="Date" sortable="custom"></el-table-column>
+    <el-table-column prop="fulfillmentStaus" label="Fulfillment Staus">
+      <template scope="scope">
+       <el-tag>{{scope.row.fulfillmentStaus}}</el-tag>
+     </template>
     </el-table-column>
-    <el-table-column
-      prop="status"
-      label="Status">
+    <el-table-column prop="paymentStatus" label="Payment Staus">
+      <template scope="scope">
+       <el-tag  :type="scope.row.paymentStatus == 'paid' ? 'danger' : 'success'">{{scope.row.paymentStatus}}</el-tag>
+      </template>
     </el-table-column>
+    <el-table-column prop="total" label="Total"></el-table-column>
+    <el-table-column prop="customerID" label="Customer ID">
+
+    </el-table-column>
+
   </data-tables>
 </template>
 
@@ -22,118 +32,127 @@ export default {
     return {
       tableData: [
         {
-          orderID: '3423424',
-          status: 'dfsdf'
+          orderID: '123213',
+          date: '2016-05-03',
+          fulfillmentStaus: 'pending',
+          paymentStatus: 'unpaid',
+          total: '540',
+          customerID: '45645'
         },
         {
-          orderID: '565464',
-          status: 'xcvxv'
+          orderID: '67567',
+          date: '2016-10-10',
+          fulfillmentStaus: 'processing',
+          paymentStatus: 'unpaid',
+          total: '540',
+          customerID: '45645'
         },
         {
-          orderID: '3423424',
-          status: 'dfsdf'
+          orderID: '123213',
+          date: '2016-05-03',
+          fulfillmentStaus: 'pending',
+          paymentStatus: 'paid',
+          total: '540',
+          customerID: '45645'
         },
         {
-          orderID: '565464',
-          status: 'xcvxv'
+          orderID: '67567',
+          date: '2016-10-10',
+          fulfillmentStaus: 'processing',
+          paymentStatus: 'unpaid',
+          total: '540',
+          customerID: '45645'
         },
         {
-          orderID: '3423424',
-          status: 'dfsdf'
+          orderID: '123213',
+          date: '2016-05-03',
+          fulfillmentStaus: 'pending',
+          paymentStatus: 'unpaid',
+          total: '540',
+          customerID: '45645'
         },
         {
-          orderID: '565464',
-          status: 'xcvxv'
+          orderID: '123213',
+          date: '2016-05-03',
+          fulfillmentStaus: 'pending',
+          paymentStatus: 'unpaid',
+          total: '540',
+          customerID: '45645'
         },
         {
-          orderID: '3423424',
-          status: 'dfsdf'
+          orderID: '67567',
+          date: '2016-10-10',
+          fulfillmentStaus: 'processing',
+          paymentStatus: 'unpaid',
+          total: '540',
+          customerID: '45645'
         },
         {
-          orderID: '565464',
-          status: 'xcvxv'
+          orderID: '123213',
+          date: '2016-05-03',
+          fulfillmentStaus: 'pending',
+          paymentStatus: 'unpaid',
+          total: '540',
+          customerID: '45645'
         },
         {
-          orderID: '3423424',
-          status: 'dfsdf'
-        },
-        {
-          orderID: '565464',
-          status: 'xcvxv'
-        },
-        {
-          orderID: '3423424',
-          status: 'dfsdf'
-        },
-        {
-          orderID: '565464',
-          status: 'xcvxv'
+          orderID: '123213',
+          date: '2016-05-03',
+          fulfillmentStaus: 'pending',
+          paymentStatus: 'unpaid',
+          total: '540',
+          customerID: '45645'
         },{
-          orderID: '3423424',
-          status: 'dfsdf'
+          orderID: '123213',
+          date: '2016-05-03',
+          fulfillmentStaus: 'pending',
+          paymentStatus: 'unpaid',
+          total: '540',
+          customerID: '45645'
         },
         {
-          orderID: '565464',
-          status: 'xcvxv'
+          orderID: '123213',
+          date: '2016-05-03',
+          fulfillmentStaus: 'pending',
+          paymentStatus: 'unpaid',
+          total: '540',
+          customerID: '45645'
         },
-        {
-          orderID: '3423424',
-          status: 'dfsdf'
-        },
-        {
-          orderID: '565464',
-          status: 'xcvxv'
-        },
-        {
-          orderID: '3423424',
-          status: 'dfsdf'
-        },
-        {
-          orderID: '565464',
-          status: 'xcvxv'
-        },
-        {
-          orderID: '3423424',
-          status: 'dfsdf'
-        },
-        {
-          orderID: '565464',
-          status: 'xcvxv'
-        },
-        {
-          orderID: '3423424',
-          status: 'qqqqq'
-        },
-        {
-          orderID: '565464',
-          status: 'xcvxv'
-        },
+
       ]
     }
   },
   methods: {
-    getRowActionsDef() {
-      let self = this
-      return [{
-        type: 'primary',
-        handler(row) {
-          self.$message('Edit clicked')
-          console.log('Edit in row clicked', row)
-        },
-        name: 'Edit'
-      }, {
-        type: 'primary',
-        handler(row) {
-          self.$message('RUA in row clicked')
-          console.log('RUA in row clicked', row)
-        },
-        name: 'RUA'
-      }]
-    },
+    // getRowActionsDef() {
+    //   let self = this
+    //   return [{
+    //     type: 'primary',
+    //     handler(row) {
+    //       self.$message('Edit clicked')
+    //       console.log('Edit in row clicked', row)
+    //     },
+    //     name: 'Edit'
+    //   }, {
+    //     type: 'primary',
+    //     handler(row) {
+    //       self.$message('RUA in row clicked')
+    //       console.log('RUA in row clicked', row)
+    //     },
+    //     name: 'RUA'
+    //   }]
+    // },
     rowClick(row) {
      this.$message('row clicked')
      console.log('row clicked', row)
    },
+   getPaginationDef() {
+     return {
+       layout: 'total, prev, pager, next, jumper, sizes',
+       pageSize: 10,
+       pageSizes: [10, 20, 50],
+      //  currentPage: 2
+     }
+    }
 
   }
 }
