54de7fb574b3569664ba8450fbc685d11d4e5d9d
Hoang Nam <namhoang.td09@gmail.com>
2017-02-21 08:07:05 +0000

    update delete function

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="">
     <el-row :gutter="30">
-      <el-col :span="16" >
+      <el-col :span="2">
+        <el-button type="primary" @click="handleClickMultiDelete">
+          <i class="el-icon-delete el-icon-right"></i>
+        </el-button>
+      </el-col>
+      <el-col :span="14">
         <el-input  placeholder="Search products"  icon="search" v-model="input" :on-icon-click="handleSearchClick"></el-input>
       </el-col>
       <el-col :span="4" :offset="4">
@@ -14,10 +19,10 @@
     <el-row>
       <el-table
         :data="tableData"
-        border
-        style="width: 100%">
+        border style="width: 100%"
+        @selection-change="handleSelectionChange">
         <!-- <el-table-column prop="imgTag" label="" width="100"></el-table-column> -->
-        <el-table-column type="index" width="50"></el-table-column>
+        <el-table-column type="selection" width="50"></el-table-column>
         <el-table-column width="100">
           <template scope="scope">
             <img :src="scope.row.imgURL" alt="">
@@ -62,6 +67,39 @@ export default {
       // console.log(keyword)
       this.tableData = data;
     },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+
+      // console.log(val.name);
+    },
+    handleClickMultiDelete() {
+      console.log("delete hihi");
+      this.$confirm('This will permanently delete the product. Continue?', 'Warning', {
+          confirmButtonText: 'OK',
+          cancelButtonText: 'Cancel',
+          type: 'warning'
+        }).then(() => {
+
+          let data = this.tableData;
+          for(var item of this.multipleSelection) {
+            data = data.filter((el) => {
+              return el.id != item.id
+            })
+          }
+          this.tableData = data;
+
+          this.$message({
+            type: 'success',
+            message: 'Delete completed'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: 'Delete canceled'
+          });
+        });
+
+    },
     handleClickEdit() {
       console.log('click');
     },
@@ -87,6 +125,7 @@ export default {
     return {
       tableData: [
         {
+          id: 0,
           name: 'Acer Aspire E 15 E5-575-33BM',
           category: 'Computers',
           brand: 'Acer',
@@ -95,6 +134,7 @@ export default {
           imgURL: "https://images-na.ssl-images-amazon.com/images/I/711XyKNjr6L._SL1331_.jpg"
         },
         {
+          id: 1,
           name: 'HP Notebook 15-ay011nr',
           category: 'Computers',
           brand: 'HP',
@@ -103,6 +143,7 @@ export default {
           imgURL: "https://images-na.ssl-images-amazon.com/images/I/714TSsHtZNL._SL1500_.jpg"
         },
         {
+          id: 2,
           name: 'NETGEAR Nighthawk AC1750',
           category: 'Computers',
           brand: 'NETGEAR',
@@ -111,6 +152,7 @@ export default {
           imgURL: "https://images-na.ssl-images-amazon.com/images/I/91-tQAAX4jL._SL1500_.jpg"
         },
         {
+          id: 3,
           name: 'CYBERPOWERPC Gamer Xtreme VR GXiVR8020A Gaming Desktop',
           category: 'Computers',
           brand: 'CYBERPOWERPC',
@@ -119,6 +161,7 @@ export default {
           imgURL: "https://images-na.ssl-images-amazon.com/images/I/81nmXFn%2BbDL._SL1500_.jpg"
         },
         {
+          id: 4,
           name: 'Apple 42mm Smart Watch',
           category: 'Cell Phones',
           brand: 'Apple',
@@ -127,6 +170,7 @@ export default {
           imgURL: "https://images-na.ssl-images-amazon.com/images/I/61WzbpVKIdL._SL1500_.jpg"
         },
         {
+          id: 5,
           name: 'Samsung Galaxy S7 ',
           category: 'Cell Phones',
           brand: 'Samsung',
@@ -135,6 +179,7 @@ export default {
           imgURL: "https://images-na.ssl-images-amazon.com/images/I/71xmfDEzOuL._SL1500_.jpg"
         },
         {
+          id: 6,
           name: 'LG Electronics 65UH8500 65-Inch 4K Smart TV',
           category: 'TV',
           brand: 'Samsung',
@@ -143,6 +188,7 @@ export default {
           imgURL: "https://images-na.ssl-images-amazon.com/images/I/91P76LdcFOL._SL1500_.jpg"
         },
         {
+          id: 7,
           name: 'Apple iPhone 7 128GB',
           category: 'Cell Phones',
           brand: 'Apple',
@@ -151,6 +197,7 @@ export default {
           imgURL: "http://pisces.bbystatic.com/image2/BestBuy_US/images/products/5581/5581593_sd.jpg;maxHeight=1000;maxWidth=1000"
         }
       ],
+      multipleSelection: [],
       input: ''
     }
   }
