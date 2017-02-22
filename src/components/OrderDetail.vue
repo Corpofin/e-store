0471b20f6477b20a07496da1dab58395f41f7e9c
Hoang Nam <namhoang.td09@gmail.com>
2017-02-22 20:19:00 +0000

    update Order detail component

--- src/components/OrderDetail.vue
+++ src/components/OrderDetail.vue
@@ -1,7 +1,11 @@
 <style scoped>
 
+.el-col {
+    margin-bottom: 20px;
+}
+
 h3 {
-  margin: 0;
+    margin: 0;
 }
 
 p.title {
@@ -9,18 +13,34 @@ p.title {
     font-size: 18px;
 }
 
-div.section {
-    height: 100px;
-    margin-top: 20px;
+.customer-card p {
+    margin: 10px;
 }
 
-.update {
-    height: 100%;
+.customer-card .section {
+    height: auto;
+    margin-bottom: 30px;
+}
+
+div.section {
+    height: 100px;
+    margin-top: 10px;
 }
 
 .update button {
     float: right;
-    margin-bottom: 20px;
+}
+
+.total {
+  margin-top: 20px;
+  font-weight: bold;
+}
+
+.total p {
+  float: right;
+  margin-right: 40px;
+  margin-top: 0;
+
 }
 
 </style>
@@ -31,7 +51,7 @@ div.section {
     <el-col :span='14'>
         <el-card class="box-card">
             <div slot="header" class="clearfix">
-                <h3>Details</h3>
+                <h3>Order Details</h3>
             </div>
 
             <el-col :span="12">
@@ -70,7 +90,7 @@ div.section {
             </el-col>
             <el-col :span="24">
                 <div class="update">
-                    <el-button type="primary">Update</el-button>
+                    <el-button type="primary"  @click='update' >Update</el-button>
                 </div>
 
             </el-col>
@@ -79,9 +99,53 @@ div.section {
 
     </el-col>
     <el-col :span='10'>
-        <el-card class="box-card">
-            <h3>Customer Details</h3>
+        <el-card class="customer-card">
+            <div slot="header" class="clearfix">
+                <h3>Customer Details</span>
+            </div>
+
+            <div class="">
+                <div class="section">
+                  <p class="title">Name</p>
+                  <p>{{this.customer.name}}</p>
+                </div>
+                <div class="section">
+                  <p class="title">Address</p>
+                  <p>{{this.customer.address}}</p>
+                </div>
+                <div class="section">
+                  <p class="title">Email</p>
+                  <p>{{this.customer.email}}</p>
+                </div>
+            </div>
+
         </el-card>
+
+
+    </el-col>
+
+    <el-col :span='14'>
+      <el-card class="customer-card">
+          <div slot="header" class="clearfix">
+              <h3>Items</span>
+          </div>
+
+          <div class="">
+            <el-table :data="items">
+              <el-table-column type="index" width="50"></el-table-column>
+              <el-table-column prop="name" label="Name"></el-table-column>
+              <el-table-column prop="quantity" label="Quantity" width="100"></el-table-column>
+              <el-table-column prop="total" label="Total" width="100">
+                <template scope='scope'>
+                  ${{scope.row.total}}
+                </template>
+              </el-table-column>
+            </el-table>
+
+            <div class="total">
+              Order Total <p>${{ this.order.total }}</p>
+            </div>
+          </div>
     </el-col>
 </el-row>
 
@@ -90,6 +154,12 @@ div.section {
 <script>
 
 export default {
+    methods: {
+      update() {
+        console.log(this.orderValue);
+        console.log(this.shippingValue);
+      }
+    },
     data() {
         return {
             order: {
@@ -97,9 +167,26 @@ export default {
                 date: '2016-05-03',
                 fulfillmentStaus: 'pending',
                 paymentStatus: 'unpaid',
-                total: '540',
+                total: 1323,
                 customerID: '45645'
             },
+            customer: {
+                name: 'Daniel Wright',
+                email: 'daniel83@example.com',
+                address: '2822 Hoffman Avenue, Flintstone, Georgia'
+
+            },
+            items: [{
+                name: 'Smart Tivi LED Samsung 43 inch 4K',
+                quantity: 1,
+                unit_price: 528,
+                total: 525
+            }, {
+                name: 'Neato Robotics - Botvac D3 App-Controlled Robot Vacuum - Dark gray',
+                quantity: 2,
+                unit_price: 399,
+                total: 798
+            }],
             orderOptions: [{
                 value: 'pending_payment',
                 label: 'Pending payment'
@@ -136,9 +223,22 @@ export default {
                 value: 'Paypal',
                 label: 'Paypal'
             }],
+            get orderValue() {
+              return this.orderOptions[0].value
+            },
+            // set orderValue(value) {
+            //   // console.log(value);
+            //   // this.orderValue = value
+            // },
+            // get shippingValue() {
+            //   return this.shippingOptions[0].value
+            // },
+            // get paymentValue() {
+            //   return this.paymentOptions[1].value
+            // },
+            orderValue: '',
             shippingValue: '',
-            paymentValue: '',
-            orderValue: ''
+            paymentValue: ''
         }
     }
 
