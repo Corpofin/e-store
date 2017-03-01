243ffd5d893d2c42ff01daaf3861b4418b6b973d
Hoang Nam <namhoang.td09@gmail.com>
2017-03-01 23:00:14 +0000

    add delete functions

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,21 +1,21 @@
-<style lang="css">
+<style>
 
-
+.actions .el-button--primary {
+    color: #fff;
+    background-color: #ff4949;
+    border-color: #ff4949;
+}
 
 </style>
 
 <template>
 
-<data-tables :data='tableData' :has-action-col='false' :pagination-def='getPaginationDef()'>
+<data-tables :data='tableData' :pagination-def='getPaginationDef()' :actions-def='getActionsDef()' :row-action-def='getRowActionsDef()' action-col-width='50' @selection-change="handleSelectionChange">
+    <el-table-column type="selection" width="50"></el-table-column>
     <el-table-column prop="name" label="Name" sortable="custom"></el-table-column>
     <el-table-column prop="email" label="Email" sortable="custom"></el-table-column>
     <el-table-column prop="registerd_since" label="Registerd Since" sortable="custom"></el-table-column>
-    <el-table-column label="" fixed="right" width="120">
-        <template scope="scope">
-            <el-button size="small"><i class="el-icon-edit"></i></el-button>
-            <el-button @click="handleClickDelete" type="danger" size="small"><i class="el-icon-delete"></i></el-button>
-        </template>
-    </el-table-column>
+
 </data-tables>
 
 </template>
@@ -66,24 +66,83 @@ export default {
                 name: 'Phan Quỳnh',
                 email: 'phanquynh@example.com',
                 registerd_since: '2017-01-18T12:53:43.947Z'
-            }]
+            }],
+            multipleSelection: []
         }
     },
     methods: {
-        handleClickEdit() {
+        handleSelectionChange(val) {
+                this.multipleSelection = val;
 
-        },
-        handleClickDelete() {
+                console.log(val.name);
+            },
+            getActionsDef() {
+                let self = this
+                return {
+                    width: 5,
+                    def: [{
+                        type: 'danger',
+                        name: 'delete',
+                        handler() {
 
-        },
+                            console.log("delete hihi");
+                            self.$confirm('This will permanently delete the product. Continue?', 'Warning', {
+                                confirmButtonText: 'OK',
+                                cancelButtonText: 'Cancel',
+                                type: 'warning'
+                            }).then(() => {
 
-        getPaginationDef() {
-            return {
-                layout: 'total, prev, pager, next, jumper, sizes',
-                pageSize: 10,
-                pageSizes: [10, 20, 50],
+                                let data = self.tableData;
+                                for (var item of self.multipleSelection) {
+                                    data = data.filter((el) => {
+                                        return el.userID != item.userID
+                                    })
+                                }
+                                self.tableData = data;
+
+                                self.$message({
+                                    type: 'success',
+                                    message: 'Delete completed'
+                                });
+                            }).catch(() => {
+                                self.$message({
+                                    type: 'info',
+                                    message: 'Delete canceled'
+                                });
+                            });
+
+                        },
+                        icon: 'delete'
+                    }]
+                }
+            },
+
+            getRowActionsDef() {
+                let self = this
+                return [{
+                    type: 'primary',
+                    handler(row) {
+                        self.$message('Edit clicked')
+                        console.log('Edit in row clicked', row)
+                    },
+                    name: 'Edit'
+                }, {
+                    type: 'danger',
+                    handler(row) {
+                        self.$message('Delete in row clicked')
+                        console.log('Delete in row clicked', row)
+                    },
+                    name: 'Delete'
+                }]
+            },
+
+            getPaginationDef() {
+                return {
+                    layout: 'total, prev, pager, next, jumper, sizes',
+                    pageSize: 10,
+                    pageSizes: [10, 20, 50],
+                }
             }
-        }
 
     }
 }
